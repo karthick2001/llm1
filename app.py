e0286fe7530d3c1063e8148d6a9018ceb4b8fcdb
karthick <vikramkarthick2001@gmail.com>
2024-09-29 11:24:33 +0530

    for ec2

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def user_input(user_question):
         return
     
     # Load the FAISS index
-    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+    new_db = FAISS.load_local("faiss_index", embeddings,allow_dangerous_deserialization=True)
     
     docs = new_db.similarity_search(user_question)
     
