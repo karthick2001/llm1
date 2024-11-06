1db8fe0a3fea2733bd81f2bcde14c1323b3ec4a8
karthick <vikramkarthick2001@gmail.com>
2024-11-06 12:58:42 +0530

    codedeployscripts1

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-echo "Stopping any running containers..."
-docker stop $(docker ps -q) || true
-docker rm $(docker ps -a -q) || true
+
+
+echo "Stopping running Docker containers..."
+docker ps -q | xargs -r docker stop
+
+echo "Removing stopped Docker containers..."
+docker ps -a -q | xargs -r docker rm
+
+echo "Removing Docker images..."
+docker images -q | xargs -r docker rmi
