d1ed39b550a80f8347c457433cd36056047a73b8
karthick <vikramkarthick2001@gmail.com>
2024-11-07 16:32:02 +0530

    llm1

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-
 echo "Stopping running Docker containers..."
-docker ps -q | xargs -r docker stop
+docker ps -q | xargs -r docker stop || true
 
 echo "Removing stopped Docker containers..."
-docker ps -a -q | xargs -r docker rm
+docker ps -a -q | xargs -r docker rm || true
 
 echo "Removing Docker images..."
-docker images -q | xargs -r docker rmi
+docker images -q | xargs -r docker rmi -f || true
